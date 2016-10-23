2ac17011282ee9feae53b4d848f165a388345b27
Tyler Whitsett <whitsett.tyler@gmail.com>
2016-10-22 21:37:54 -0700

    add users controler for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_any_scope!
+
+  def index
+    @users = User.all
+    authorize User
+  end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.update(user_params)
+      redirect_to user_path(@user), success: "User updated successfully!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
 end
