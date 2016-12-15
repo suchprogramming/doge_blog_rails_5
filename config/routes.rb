7ba1dbd7b6e21123871c75bb3e619ccbe03aa152
Tyler Whitsett <whitsett.tyler@gmail.com>
2016-12-14 18:20:35 -0800

    remove nested index post route

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
 
   # Admin routes for Posts
   resources :admins, only: [:index, :show] do
-    resources :posts
+    resources :posts, except: [:index]
   end
 
   # Admin routes for user management
   scope "admins/manage", as: "admin_manage" do
-    resources :users, only: [:index, :edit, :update, :destroy]
+    resources :users, only: [:edit, :update, :destroy]
   end
 
   # Devise user routes
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
   # User routes for Posts
   resources :users, only: [:show] do
-    resources :posts
+    resources :posts, except: [:index]
   end
 
 end
