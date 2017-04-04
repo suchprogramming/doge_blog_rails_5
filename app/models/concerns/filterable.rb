88274066f4921d599d5cc330f8b060799089d0e4
Tyler Whitsett <whitsett.tyler@gmail.com>
2017-04-04 12:36:41 -0700

    minor styling changes for the post filters

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -6,7 +6,7 @@ module Filterable
     def filter(filtering_params)
       results = self.where(nil)
       filtering_params.each do |key, value|
-        results = results.public_send(key, value) if value.present?
+        results = results.public_send(key, value.strip) if value.present?
       end
       results
     end
--- spec/features/search/user_searches_for_post_spec.rb
+++ spec/features/search/user_searches_for_post_spec.rb
@@ -9,38 +9,37 @@ RSpec.feature 'User searches for a post', js: true do
   end
 
   scenario 'with success' do
-    fill_in 'post_search', with: post.title
-    click_on 'Search'
+    find("input#post_search").set("wrong title\n")
+
+    expect(page).not_to have_selector('h5', text: post.title)
+
+    find("input#post_search").set("#{post.title}\n")
 
     expect(page).to have_selector('h5', text: post.title)
   end
 
   scenario 'with no posts found' do
-    fill_in 'post_search', with: 'Wrong Title!'
-    click_on 'Search'
+    find("input#post_search").set("wrong title\n")
 
     expect(page).not_to have_selector('h5', text: post.title)
   end
 
   scenario 'with the last day filter applied' do
-    fill_in 'post_search', with: post.title
-    click_on 'Search'
+    find("input#post_search").set("#{post.title}\n")
     select('Last Day', from: 'post-filter-select', visible: false)
 
     expect(all('.card-panel').count).to eq(1)
   end
 
   scenario 'with the last week filter applied' do
-    fill_in 'post_search', with: post.title
-    click_on 'Search'
+    find("input#post_search").set("#{post.title}\n")
     select('Last Week', from: 'post-filter-select', visible: false)
 
     expect(all('.card-panel').count).to eq(2)
   end
 
   scenario 'with the last month filter applied' do
-    fill_in 'post_search', with: post.title
-    click_on 'Search'
+    find("input#post_search").set("#{post.title}\n")
     select('Last Month', from: 'post-filter-select', visible: false)
 
     expect(all('.card-panel').count).to eq(3)
@@ -54,8 +53,7 @@ RSpec.feature 'User searches for a post', js: true do
     expect(page).to have_text('Testing Search & Filter')
     expect(page).to have_text('Testing Filter & Then Search')
 
-    fill_in 'post_search', with: post.title
-    click_on 'Search'
+    find("input#post_search").set("#{post.title}\n")
 
     expect(page).not_to have_text('Testing Search & Filter')
     expect(page).to have_text('Testing Filter & Then Search')
